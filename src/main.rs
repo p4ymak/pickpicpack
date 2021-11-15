f80ed71ab211300c7c41a31a5f557fa5aede7bf2
Roman Chumak <p4ymak@gmail.com>
2021-11-15 16:06:22 +0300

    Release 0.1.4

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,6 @@ mod utils;
 use app::*;
 
 fn main() {
-    let icon = eframe::epi::IconData {
-        rgba: image::open("./icon/128x128@2x.png")
-            .unwrap()
-            .to_rgba8()
-            .to_vec(),
-        width: 256,
-        height: 256,
-    };
     let start_state = P3App::default();
     let options = eframe::NativeOptions {
         always_on_top: false,
@@ -23,7 +15,6 @@ fn main() {
         maximized: false,
         drag_and_drop_support: true,
         transparent: true,
-        icon_data: Some(icon),
         ..Default::default()
     };
     eframe::run_native(Box::new(start_state), options);
